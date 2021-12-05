c9453b0bf49edd969cb35f666b8c0872a71025c0
HristakaZ <hristaka2013@abv.bg>
2021-12-04 22:09:28 -0800

    Added score count, restart game functionality, refactored some code, made the game easier by widening the walls and respawning them at the end of the screen. Added song to play while playing the game.

--- FlappyBird/FlappyBird/GameScene.swift
+++ FlappyBird/FlappyBird/GameScene.swift
@@ -7,6 +7,7 @@
 
 import SpriteKit
 import GameplayKit
+import AVFoundation
 
 struct CollisionBitMask {
     //we use these bitmasks to see if:
@@ -17,9 +18,10 @@ struct CollisionBitMask {
     static let Bird : UInt32 = 1
     static let Ground : UInt32 = 2
     static let Wall : UInt32 = 3
+    static let MiddleOfWallPair : UInt32 = 4
 }
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var background = SKSpriteNode(imageNamed: "Flappy-Bird-Background")
     var ground = SKSpriteNode(imageNamed: "Flappy-Bird-Ground")
@@ -27,6 +29,11 @@ class GameScene: SKScene {
     var wallPair = SKNode()
     var moveAndRemoveAction = SKAction()
     var gameStarted = Bool()
+    var score = Int()
+    var scoreLabel = SKLabelNode()
+    var isDead = Bool()
+    var restartButton = SKSpriteNode(imageNamed: "Flappy-Bird-Restart-Button")
+    var audioPlayer = AVAudioPlayer()
     
     override func didMove(to view: SKView) {
         /*removing the helloLabel from the gamescene as we cannot remove it manually from the file using a VM because xcode shuts down unexpectedly(lol)
@@ -34,63 +41,108 @@ class GameScene: SKScene {
         let helloLabel = self.childNode(withName: "helloLabel") as? SKLabelNode
         helloLabel?.removeFromParent()
         
-        createBackground()
-        createGround()
-        createBird()
+        self.physicsWorld.contactDelegate = self
+        
+        createMap()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         //begin falling only when we start touching the screen
         bird.physicsBody?.affectedByGravity = true
-        bird.run(SKAction.moveTo(x: self.frame.midX, duration: 1.0))
         if gameStarted == false {
+            
             gameStarted = true
             
-            var moveWalls = SKAction()
-            var removeWalls = SKAction()
-            let distance = CGFloat(self.frame.width + wallPair.frame.width)
-            let spawnWalls = SKAction.run({
-                () in
+            if !isDead {
+                bird.run(SKAction.moveTo(x: self.frame.midX, duration: 1.0))
                 
-                moveWalls = SKAction.moveBy(x: -distance, y: 0, duration: 2 * distance)
-                removeWalls = SKAction.removeFromParent()
-                self.createWallPair()
-            })
-            
-            let spawnWallsDelay = SKAction.wait(forDuration: 1.5)
-            let spawnWallsSequence = SKAction.sequence([spawnWalls, spawnWallsDelay])
-            let spawnWallsDelayForever = SKAction.repeatForever(spawnWallsSequence)
-            self.run(spawnWallsDelayForever)
-            
-            moveAndRemoveAction = SKAction.sequence([moveWalls, removeWalls])
-            
-            bird.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
-            bird.physicsBody?.applyImpulse(CGVector(dx:0, dy:90))
-            self.wallPair.run(moveAndRemoveAction)
+                playSong()
+                
+                var moveWalls = SKAction()
+                var removeWalls = SKAction()
+                let distance = CGFloat(self.frame.width + wallPair.frame.width)
+                let spawnWalls = SKAction.run({
+                    () in
+                    
+                    if !self.isDead {
+                        moveWalls = SKAction.moveBy(x: -distance, y: 0, duration: 2 * distance)
+                        removeWalls = SKAction.removeFromParent()
+                        self.createWallPair()
+                    }
+                })
+                
+                let spawnWallsDelay = SKAction.wait(forDuration: 1.5)
+                let spawnWallsSequence = SKAction.sequence([spawnWalls, spawnWallsDelay])
+                let spawnWallsDelayForever = SKAction.repeatForever(spawnWallsSequence)
+                self.run(spawnWallsDelayForever)
+                
+                moveAndRemoveAction = SKAction.sequence([moveWalls, removeWalls])
+                
+                bird.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+                bird.physicsBody?.applyImpulse(CGVector(dx:0, dy:90))
+                self.wallPair.run(moveAndRemoveAction)
+            }
         }
         else {
-            let distance = CGFloat(self.frame.width + wallPair.frame.width)
-            let moveWalls = SKAction.moveBy(x: -distance, y: 0, duration: 0.008 * distance)
-            let removeWalls = SKAction.removeFromParent()
-            moveAndRemoveAction = SKAction.sequence([moveWalls, removeWalls])
             
-            bird.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
-            bird.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 190))
-            self.wallPair.run(moveAndRemoveAction)
+            if !isDead {
+                let distance = CGFloat(self.frame.width + wallPair.frame.width)
+                let moveWalls = SKAction.moveBy(x: -distance, y: 0, duration: 0.008 * distance)
+                let removeWalls = SKAction.removeFromParent()
+                moveAndRemoveAction = SKAction.sequence([moveWalls, removeWalls])
+                
+                bird.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+                bird.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 190))
+                self.wallPair.run(moveAndRemoveAction)
+            }
+            
+            for touch in touches {
+                let locationOfTouch = touch.location(in: self)
+                
+                if isDead {
+                    if restartButton.contains(locationOfTouch) {
+                        restartMap()
+                    }
+                }
+            }
         }
         
     }
     
+    func didBegin(_ contact: SKPhysicsContact) {
+        changeScore(contact)
+        
+        tryDying(contact)
+    }
+    
+    func createMap() {
+        createBackground()
+        createGround()
+        createBird()
+        createScore()
+    }
+    
+    func restartMap() {
+        self.removeAllChildren()
+        self.removeAllActions()
+        isDead = false
+        gameStarted = false
+        score = 0
+        createMap()
+    }
+    
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
-        let endlesslyMoveWallsAction = SKAction.run({
-            () in
-            self.endlesslyMoveWalls()
-        })
-        let delay = SKAction.wait(forDuration: 3.0)
-        let removeWalls = SKAction.removeFromParent()
-        let sequence = SKAction.sequence([endlesslyMoveWallsAction, delay, removeWalls])
-        self.wallPair.run(sequence)
+        if !isDead {
+            let endlesslyMoveWallsAction = SKAction.run({
+                () in
+                self.endlesslyMoveWalls()
+            })
+            let delay = SKAction.wait(forDuration: 3.0)
+            let removeWalls = SKAction.removeFromParent()
+            let sequence = SKAction.sequence([endlesslyMoveWallsAction, delay, removeWalls])
+            self.wallPair.run(sequence)
+        }
     }
     
     func createBackground() {
@@ -114,15 +166,16 @@ class GameScene: SKScene {
     }
     
     func createBird() {
-        bird.position.x = self.frame.minX
+        bird.position = CGPoint(x: self.frame.minX, y: self.frame.midY)
         bird.zPosition = 2
         bird.setScale(0.3)
         bird.physicsBody = SKPhysicsBody(circleOfRadius: bird.frame.height / 2)
         bird.physicsBody?.categoryBitMask = CollisionBitMask.Bird
         bird.physicsBody?.collisionBitMask = CollisionBitMask.Ground | CollisionBitMask.Wall
-        bird.physicsBody?.contactTestBitMask = CollisionBitMask.Ground | CollisionBitMask.Wall
+        bird.physicsBody?.contactTestBitMask = CollisionBitMask.Ground | CollisionBitMask.Wall | CollisionBitMask.MiddleOfWallPair
         bird.physicsBody?.affectedByGravity = false
         bird.physicsBody?.isDynamic = true
+        bird.zRotation = 0.0
         self.addChild(bird)
     }
     
@@ -130,7 +183,7 @@ class GameScene: SKScene {
         wallPair = SKNode()
         let topWall = SKSpriteNode(imageNamed: "Flappy-Bird-Wall")
         let bottomWall = SKSpriteNode(imageNamed: "Flappy-Bird-Wall")
-        topWall.position = CGPoint(x: self.frame.maxX / 2, y: self.frame.maxY)
+        topWall.position = CGPoint(x: self.frame.maxX / 1.1, y: self.frame.maxY + 100)
         topWall.physicsBody = SKPhysicsBody(rectangleOf: topWall.size)
         topWall.physicsBody?.categoryBitMask = CollisionBitMask.Wall
         topWall.physicsBody?.collisionBitMask = CollisionBitMask.Bird
@@ -138,7 +191,7 @@ class GameScene: SKScene {
         topWall.physicsBody?.isDynamic = false
         topWall.physicsBody?.affectedByGravity = false
         
-        bottomWall.position = CGPoint(x: self.frame.maxX / 2, y: self.frame.minY)
+        bottomWall.position = CGPoint(x: self.frame.maxX / 1.1, y: self.frame.minY - 100)
         bottomWall.physicsBody = SKPhysicsBody(rectangleOf: topWall.size)
         bottomWall.physicsBody?.categoryBitMask = CollisionBitMask.Wall
         bottomWall.physicsBody?.collisionBitMask = CollisionBitMask.Bird
@@ -146,27 +199,88 @@ class GameScene: SKScene {
         bottomWall.physicsBody?.isDynamic = false
         bottomWall.physicsBody?.affectedByGravity = false
         
-        topWall.position = CGPoint(x: self.frame.maxX / 2, y: self.frame.maxY)
-        
-        bottomWall.position = CGPoint(x: self.frame.maxX / 2, y: self.frame.minY)
-        
         wallPair.addChild(topWall)
         wallPair.addChild(bottomWall)
         wallPair.zPosition = 3
         wallPair.position.y = CGFloat.random(in: -200...200)
         wallPair.name = "wallPair"
+        
+        let middleOfWallPair = SKSpriteNode()
+        middleOfWallPair.size = CGSize(width: 50, height: self.frame.height / 2)
+        middleOfWallPair.position = CGPoint(x: self.frame.maxX / 1.1, y: wallPair.position.y / 2)
+        middleOfWallPair.physicsBody = SKPhysicsBody(rectangleOf: middleOfWallPair.size)
+        middleOfWallPair.physicsBody?.affectedByGravity = false
+        middleOfWallPair.physicsBody?.isDynamic = false
+        middleOfWallPair.physicsBody?.categoryBitMask = CollisionBitMask.MiddleOfWallPair
+        middleOfWallPair.physicsBody?.collisionBitMask = 0
+        middleOfWallPair.physicsBody?.contactTestBitMask = CollisionBitMask.Bird
+        
+        wallPair.addChild(middleOfWallPair)
+        
         self.addChild(wallPair)
     }
     
     func endlesslyMoveWalls(){
-        self.enumerateChildNodes(withName: "wallPair", using: ({
-            (node, error) in
-            // 1
-            node.position.x -= 9
-            // 2
-            if node.position.x < -(self.scene?.size.width)! {
-                node.position.x += (self.scene?.size.width)! * 3
+        if !isDead {
+            self.enumerateChildNodes(withName: "wallPair", using: ({
+                (node, error) in
+                node.position.x -= 9
+                if node.position.x < -(self.scene?.size.width)! {
+                    node.position.x += (self.scene?.size.width)! * 3
+                }
+             }))
+        }
+    }
+    
+    func createScore() {
+        scoreLabel.text = "\(score)"
+        scoreLabel.position = CGPoint(x: self.frame.midX, y: self.frame.maxY / 1.5)
+        scoreLabel.zPosition = 5
+        scoreLabel.fontSize = 85
+        self.addChild(scoreLabel)
+    }
+    
+    func changeScore(_ contact: SKPhysicsContact) {
+        if !isDead {
+            if (contact.bodyA.categoryBitMask == CollisionBitMask.MiddleOfWallPair && contact.bodyB.categoryBitMask == CollisionBitMask.Bird) ||
+                (contact.bodyA.categoryBitMask == CollisionBitMask.Bird && contact.bodyB.categoryBitMask == CollisionBitMask.MiddleOfWallPair) {
+                score = score + 1
+                scoreLabel.text = "\(score)"
             }
-         }))
+        }
+    }
+    
+    func tryDying(_ contact: SKPhysicsContact) {
+        if ((contact.bodyA.categoryBitMask == CollisionBitMask.Bird && contact.bodyB.categoryBitMask == CollisionBitMask.Ground)
+        || (contact.bodyA.categoryBitMask == CollisionBitMask.Ground && contact.bodyB.categoryBitMask == CollisionBitMask.Bird))
+            || ((contact.bodyA.categoryBitMask == CollisionBitMask.Bird && contact.bodyB.categoryBitMask == CollisionBitMask.Wall)
+                || (contact.bodyA.categoryBitMask == CollisionBitMask.Wall && contact.bodyB.categoryBitMask == CollisionBitMask.Bird))
+        {
+            isDead = true
+            createRestartButton()
+            audioPlayer.stop()
+        }
+    }
+    
+    func createRestartButton() {
+        restartButton.removeFromParent()
+        restartButton.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        restartButton.zPosition = 6
+        self.addChild(restartButton)
+        restartButton.run(SKAction.scale(to: 1.0, duration: 1.0))
+    }
+    
+    func playSong() {
+        let song = Bundle.main.path(forResource: "Isyan Tetick - Patlamaya Devam", ofType: "mp3")
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: song!))
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback,
+                                                            mode: AVAudioSession.Mode.default,
+                                                            options: [AVAudioSession.CategoryOptions.mixWithOthers])
+            audioPlayer.play()
+        }
+        catch {
+            print("An error occured when trying to find the song!")
+        }
     }
 }
